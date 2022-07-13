455dbbbb9f1aeaf54e1b64cd8896e249de43e2ef
stance0102 <jason89010272727@gmail.com>
2022-07-13 18:37:26 +0800

    TikTok Clone used React,Next,TS

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,31 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useState, useEffect } from 'react'
+import Navbar from '../components/Navbar'
+import Sidebar from '../components/Sidebar'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  const [isSSR, setIsSSR] = useState(true);
+
+  useEffect(() => {
+    setIsSSR(false);
+  }, []);
+
+  if (isSSR) return null;
+
+  return (
+    <div className='xl:w-[95vw] m-auto overflow-hidden h-[100vh]'>
+      <Navbar />
+      <nav className="flex gap-6 md:gap-20">
+        <div className="h-[92vh] overflow-hidden xl:hover:overflow-auto">
+          <Sidebar />
+        </div>
+        <div className="mt-4 flex flex-col gap-10 overflow-auto videos flex-1 h-[88vh]">
+          <Component {...pageProps} />
+        </div>
+      </nav>
+    </div>
+  )
 }
 
 export default MyApp
