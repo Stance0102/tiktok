f0fb94b01f331250a4844712ccc94ac8fe9977b5
stance0102 <jason89010272727@gmail.com>
2022-07-25 22:12:57 +0800

    Add User,PostVideo Feature

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,35 @@
-import type { NextPage } from 'next'
 import axios from 'axios'
+import { Video } from '../types'
+import VideoCard from '../components/VideoCard'
+import NoResult from '../components/NoResult'
 
-const Home: NextPage = () => {
+interface IProps {
+  videos: Video[]
+}
+
+const Home = ({ videos }: IProps) => {
+  console.log(videos)
+
   return (
-    <div className="text-3xl font-bold underline">
-      Hello world
+    <div className="flex flex-col gap-10 videos h-full">
+      {videos.length ? (
+        videos.map((video: Video) => (
+          <VideoCard post={video} key={video._id} />
+        ))
+      ) : (
+        <NoResult text={"No Videos"} />
+      )}
     </div>
   )
 }
 
 export const getServerSideProps = async () => {
-  const response = await axios.get("http://localhost:3000/api/post")
-
-  console.log(response.data.name)
+  const { data } = await axios.get("http://localhost:3000/api/post")
 
   return {
-    props: {}
+    props: {
+      videos: data
+    }
   }
 }
 
