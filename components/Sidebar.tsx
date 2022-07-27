f8ce1c639cdbd960a49b10407ee7d44156b45009
stance0102 <jason89010272727@gmail.com>
2022-07-27 23:41:41 +0800

    Update[sideNav UI],New feature[Video Details]

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -16,18 +16,18 @@ const Sidebar = () => {
 
     return (
         <div>
-            <div className="block m-2 ml-4 mt-3 text-xl" onClick={() => setShowSidebar((prev) => !prev)}>
+            <div className="block xl:hidden m-2 ml-4 mt-3 text-xl" onClick={() => setShowSidebar((prev) => !prev)}>
                 {showSidebar ? <ImCancelCircle /> : <AiOutlineMenu />}
             </div>
             {showSidebar && (
-                <div className="lg:w-44 xl:w-400 flex flex-col justify-start mb-10 border-r-2 border-gray-100 xl:border-0 p-3">
+                <div className="xl:w-400 w-40 flex flex-col justify-start mb-10 border-r-2 border-gray-100 xl:border-0 p-3">
                     <div className="xl:border-b-2 border-gray-200 xl:pb-4">
                         <Link href="/">
                             <div className={normalLink}>
                                 <p className='text-2xl'>
                                     <AiFillHome />
                                 </p>
-                                <span className="text-xl hidden xl:block">
+                                <span className="capitalize text-xl hidden xl:block">
                                     For You
                                 </span>
                             </div>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -16,7 +16,7 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
 
   return (
     <GoogleOAuthProvider clientId={`${process.env.NEXT_PUBLIC_GOOGLE_API_TOKEN}`}>
-      <div className='xl:w-[95vw] m-auto overflow-hidden h-[100vh]'>
+      <div className='xl:w-[1200px] m-auto overflow-hidden h-[100vh]'>
         <Navbar />
         <nav className="flex gap-6 md:gap-20">
           <div className="h-[92vh] overflow-hidden xl:hover:overflow-auto">
