58b4f6f66d3daf9ebf2f27dcae4819a13f916390
stance0102 <jason89010272727@gmail.com>
2022-07-29 23:16:02 +0800

    Try to fix bug[sanity.io backend]

--- sanity-backend/schemas/schema.js
+++ sanity-backend/schemas/schema.js
@@ -1,13 +1,12 @@
 // First, we must import the schema creator
-import createSchema from 'part:@sanity/base/schema-creator'
+import createSchema from 'part:@sanity/base/schema-creator';
 
 // Then import schema types from any plugins that might expose them
-import schemaTypes from 'all:part:@sanity/base/schema-type'
-import post from "./post";
-import user from "./user";
-import comment from "./comment";
-import postedBy from "./postedBy"
-
+import schemaTypes from 'all:part:@sanity/base/schema-type';
+import post from './post';
+import user from './user';
+import comment from './comment';
+import postedBy from './postedBy';
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
   // We name our schema
@@ -15,6 +14,10 @@ export default createSchema({
   // Then proceed to concatenate our document type
   // to the ones provided by any plugins that are installed
   types: schemaTypes.concat([
-    post, user, comment, postedBy
+    /* Your types here! */
+    post,
+    user,
+    comment,
+    postedBy,
   ]),
-})
+});
